dcf2f1d447c2f177c1e3acfde38f035dbc22d413
Harsh <harszh01@gmail.com>
2026-09-26 01:40:16 +0530

    updated files

--- 2D arrays/mult_two_matrices.c
+++ 2D arrays/mult_two_matrices.c
@@ -1,16 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
-{
-    int array1[2][2]={1,2,0,1};
-    int array2[2][2]={0,1,0,1};
-    int sum=0;
-    for (int i = 0; i < 2; i++)
+        // yha se rowws or columns ko manipulate karlo..
+{   int m=3,n=2,q=4;
+    int matrix1[3][2] = {{1, 2}, {3, 4}, {5, 6}};
+    int matrix2[2][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}};
+    int result[3][4];
+
+    for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < 2; j++)
-        {
-            
+        for (int j = 0; j < q; j++)
+        {    result[i][j]=0;
+            // matrix multiplication ke liye jo row/column common hoti hai utna tak hi 'k' chalta hai..
+            for (int k = 0; k < n; k++)
+            {       // 'k' i,j pe depend nahi kar rha ye independently inc. karega from '0' ..
+                result[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
         }
     }
-    
-return 0;
+     for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < q; j++)
+        {
+           printf("%d ",result[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
